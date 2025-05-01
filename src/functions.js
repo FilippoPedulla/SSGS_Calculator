9a1ae54144e5051cbcac0ebc3892f97dfa011ced
Filippo <pippoped10@gmail.com>
2025-05-01 12:23:56 +0200

    update functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,12 +1,15 @@
 function sum (a, b) {
   return a + b;
 }
+
 function sub (a, b) {
   return a - b;
 }
+
 function mul (a, b) {
   return a * b;
 }
+
 function div (a, b) {
   if (b === 0) {
     console.log('Divisione per zero non consentita.');
@@ -14,6 +17,7 @@ function div (a, b) {
   }
   return a / b;
 }
+
 function pow (a, b) {
   return Math.pow(a, b);
 }
